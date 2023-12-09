56e981fecb7c2ea2adb2b31cc4162573821a2ce7
Kang Zhang <originalsuperarmd@gmail.com>
2023-12-09 12:12:14 -0500

    Added Comments

--- Lab6/pythonScript/readRomi.py
+++ Lab6/pythonScript/readRomi.py
@@ -12,15 +12,15 @@ MQTT_USER = "team8"
 MQTT_PASSWORD = "mons0892"
 
 # Topics
-TOPIC_ID0 = ["team8/tag0/x", "team8/tag0/y", "team8/tag0/rot"]
-TOPIC_ID1 = ["team8/tag1/x", "team8/tag1/y", "team8/tag1/rot"]
-TOPIC_ID2 = ["team8/tag2/x", "team8/tag2/y", "team8/tag2/rot"]
+TOPIC_ID0 = ["theIlluminati/tag0/x", "theIlluminati/tag0/y", "theIlluminati/tag0/rot", "theIlluminati/tag0/id"]
+TOPIC_ID1 = ["theIlluminati/tag1/x", "theIlluminati/tag1/y", "theIlluminati/tag1/rot", "theIlluminati/tag1/id"]
+TOPIC_ID2 = ["theIlluminati/tag2/x", "theIlluminati/tag2/y", "theIlluminati/tag2/rot", "theIlluminati/tag2/id"]
 
 # Initialize data structure
 tag_data = {
-    TOPIC_ID0[0]: -200.0, TOPIC_ID0[1]: -200.0, TOPIC_ID0[2]: 0.0, # x, y, angle
-    TOPIC_ID1[0]: -200.0, TOPIC_ID1[1]: -200.0, TOPIC_ID1[2]: 0.0,
-    TOPIC_ID2[0]: -200.0, TOPIC_ID2[1]: -200.0, TOPIC_ID2[2]: 0.0
+    TOPIC_ID0[0]: -200.0, TOPIC_ID0[1]: -200.0, TOPIC_ID0[2]: 0.0, TOPIC_ID0[3]: -1, # x, y, angle, ID
+    TOPIC_ID1[0]: -200.0, TOPIC_ID1[1]: -200.0, TOPIC_ID1[2]: 0.0, TOPIC_ID1[3]: -1,
+    TOPIC_ID2[0]: -200.0, TOPIC_ID2[1]: -200.0, TOPIC_ID2[2]: 0.0, TOPIC_ID2[3]: -1
 }
 
 # Create a queue
@@ -35,16 +35,17 @@ def on_message(client, userdata, msg):
     x_vec = [tag_data[TOPIC_ID0[0]], tag_data[TOPIC_ID1[0]], tag_data[TOPIC_ID2[0]]]
     y_vec = [tag_data[TOPIC_ID0[1]], tag_data[TOPIC_ID1[1]], tag_data[TOPIC_ID2[1]]]
     angles = [tag_data[TOPIC_ID0[2]], tag_data[TOPIC_ID1[2]], tag_data[TOPIC_ID2[2]]]
+    id = [tag_data[TOPIC_ID0[3]], tag_data[TOPIC_ID1[3]], tag_data[TOPIC_ID2[3]]]
     
-    data_queue.put((x_vec, y_vec, angles)) # put the data into the queue
+    data_queue.put((x_vec, y_vec, angles, id)) # put the data into the queue
 
 # Callback when the client receives a CONNACK response from the server
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-    client.subscribe("team8/tag0/#")
-    client.subscribe("team8/tag1/#")
-    client.subscribe("team8/tag2/#")
-
+    client.subscribe("theIlluminati/tag0/#")
+    client.subscribe("theIlluminati/tag1/#")
+    client.subscribe("theIlluminati/tag2/#")
+ 
 # Set up the figure and axis
 fig, ax = plt.subplots(figsize=(10, 6))
 ax.set_xlabel('X Axis')
@@ -60,9 +61,9 @@ arrows = [ax.quiver([], [], [], [], angles='xy', scale_units='xy', scale=1) for 
 # Function to update the plot
 def update_plot(frame):
     try:
-        x_vec, y_vec, angles = data_queue.get_nowait() # get the data from the queue
+        x_vec, y_vec, angles, id = data_queue.get_nowait() # get the data from the queue
         data_queue.queue.clear() # clear the queue (helps update the plot in real time by removing a backlog of data)
-        print(x_vec[0], y_vec[0], angles[0])
+        print(x_vec[0], y_vec[0], angles[0], id[0])
 
         for i in range(3): # update the lines and arrows
             lines[i].set_data(x_vec[i], y_vec[i]) 
@@ -70,7 +71,7 @@ def update_plot(frame):
             scale_factor = 1000
             dx = scale_factor * np.cos(angles[i] * np.pi / 180) 
             dy = scale_factor * np.sin(angles[i] * np.pi / 180)
-            arrows[i] = ax.quiver(x_vec[i], y_vec[i], dx, dy, angles='xy', scale_units='xy', scale=80, color=lines[i].get_color())
+            arrows[i] = ax.quiver(x_vec[i], y_vec[i], dx, dy, angles='xy', scale_units='xy', scale=80, color=lines[i].get_color()) # generate the arrows for points
        
         return lines + arrows
     except queue.Empty: # if the queue is empty, return the previous lines and arrows
